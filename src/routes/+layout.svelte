c062e9b10207a4f7e5b43bc4c9e61dfa201ac9f6
EZSNoVa <thenovaggamer@gmail.com>
2023-11-05 12:47:06 -0500

    Fixed taildwind styles, using picocss lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
-	import "$lib/css/app.css";
+	import "@picocss/pico/css/pico.css";
+	import "../app.css";
 
 	import { SvelteToast } from "@zerodevx/svelte-toast";
 
@@ -25,13 +26,7 @@
 	const transitionOut = { easing: cubicIn, y: -y, duration };
 </script>
 
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"
-		
-	/>
-</svelte:head>
+
 
 {#if isLoading}
 	<BarLoader />
